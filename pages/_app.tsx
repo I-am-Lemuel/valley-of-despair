77ae044bac144ff79311d72bb648f6075f3f3592
Xaohs <julianroeland@gmail.com>
2022-09-06 15:10:22 +0200

    s

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../src/components/Layout';
+import GlobalStyle from '../src/styles/GlobalStyles';
 // import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Layout>
+			<GlobalStyle />
 			<Head>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
