d8381d19a34e165f55d65fcf1dfb03ea67117870
SudoLemuel <lembak03@hotmail.com>
2022-09-07 16:13:06 +0200

    Styeld site blocks

--- src/components/SiteBlocks/StyledSiteBlocks.tsx
+++ src/components/SiteBlocks/StyledSiteBlocks.tsx
@@ -4,13 +4,11 @@ export const StyledSiteBlocks = styled.div`
 
 `;
 export const StyledSiteBtn = styled.button<{ selected: boolean }>`
+
 ${({ selected }) =>
     selected
-        ? `
+        && `
     border: 2px solid #000;
-    `
-        : `
-    border: 2px solid #fff;
     `}
 
 
