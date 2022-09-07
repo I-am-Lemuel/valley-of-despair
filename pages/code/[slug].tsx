9f35e0b3c4c6c9e3faca4e619169165d417a2ace
SudoLemuel <lembak03@hotmail.com>
2022-09-07 14:48:17 +0200

    gg

--- pages/code/[slug].tsx
+++ pages/code/[slug].tsx
@@ -56,7 +56,7 @@ export async function getServerSideProps(context: any) {
 					site: 'https://cssreference.io',
 				},
 				{
-					image: 'stack-overflow.png',
+					image: 'stack.png',
 					site: 'https://stackoverflow.com',
 				},
 				{
