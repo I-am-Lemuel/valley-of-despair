056494e42f50f74169e0b30dc0b3b3bdd89c9a79
Xaohs <julianroeland@gmail.com>
2022-09-07 14:53:22 +0200

    s

--- pages/code/[slug].tsx
+++ pages/code/[slug].tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import Image from 'next/future/image';
-import Link from 'next/link';
+import { useState } from 'react';
+import { default_sites } from '../../src/assets/code_sites';
 import Layout from '../../src/components/Layout';
 import { Search } from '../../src/components/Search/Search';
 interface Props {
@@ -9,17 +10,17 @@ interface Props {
 }
 const CodePage: NextPage<Props> = (props) => {
 	const { slug, stringified_sites } = props;
-	const default_sites: { image: string; site: string }[] =
-		JSON.parse(stringified_sites);
+	const all_sites: { image: string; site: string }[] = JSON.parse(stringified_sites);
 	const site_params = `\"${slug}\"`;
 
-	console.log(default_sites);
+	const [selectedSites, setSelectedSites] = useState<number[]>([1, 2, 3, 4]);
+
 	return (
 		<Layout title={`PHP - ${slug}`}>
 			<Search placeholder={`Search in ${slug}`} site_params={site_params} />
 			<h1>Slug: {slug}</h1>
 			<h1>Key params: {site_params}</h1>
-			{default_sites.map((site, index) => (
+			{all_sites.map((site, index) => (
 				<div key={index}>
 					<Image
 						src={`/${site.image}`}
@@ -35,192 +36,15 @@ const CodePage: NextPage<Props> = (props) => {
 
 export async function getServerSideProps(context: any) {
 	const { slug } = context.params;
-	const default_sites: { key: string; sites: { image: string; site: string }[] }[] = [
-		{
-			key: 'css',
-			sites: [
-				{
-					image: 'css-tricks.png',
-					site: 'https://css-tricks.com',
-				},
-				{
-					image: 'mozilla.png',
-					site: 'https://developer.mozilla.org',
-				},
-				{
-					image: 'w3.png',
-					site: 'https://www.w3schools.com',
-				},
-				{
-					image: 'css-ref.png',
-					site: 'https://cssreference.io',
-				},
-				{
-					image: 'stack-overflow.png',
-					site: 'https://stackoverflow.com',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://www.reddit.com',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://www.quora.com',
-				},
-			],
-		},
-		{
-			key: 'js',
-			sites: [
-				{
-					image: 'mozilla.png',
-					site: 'https://developer.mozilla.org',
-				},
-				{
-					image: 'W3schools.png',
-					site: 'https://w3schools.com',
-				},
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-				},
-			],
-		},
-		{
-			key: 'HTML',
-			sites: [
-				{
-					image: 'mozilla.png',
-					site: 'https://developer.mozilla.org',
-				},
-				{
-					image: 'W3schools.png',
-					site: 'https://w3schools.com',
-				},
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-				},
-			],
-		},
-		{
-			key: 'Python',
-			sites: [
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-				},
-				{
-					image: 'docs.png',
-					site: 'https://docs.python.org',
-				},
-			],
-		},
-		{
-			key: 'C',
-			sites: [
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-				},
-				{
-					image: 'gnu.png',
-					site: 'https://gnu.org',
-				},
-			],
-		},
-		{
-			key: 'PHP',
-			sites: [
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-				},
-				{
-					image: 'phpnet.png',
-					site: 'https://php.net',
-				},
-			],
-		},
-	];
 
-	const sites = default_sites.map((object, index) => {
-		if (object.key === slug) {
-			return {
-				slug: slug,
-				default_sites: default_sites[index],
-			};
-		}
-	});
+	const stringified_sites = JSON.stringify(
+		default_sites.filter((site) => site.key === slug)[0].sites,
+	);
 
 	return {
 		props: {
 			slug: slug,
-			stringified_sites: JSON.stringify(sites[0]?.default_sites.sites),
+			stringified_sites: stringified_sites,
 		},
 	};
 }
