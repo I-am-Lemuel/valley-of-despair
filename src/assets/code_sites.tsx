7ef7087e19d264c4e2b7ad388831c7e2f965b0bf
Xaohs <julianroeland@gmail.com>
2022-09-07 14:55:04 +0200

    f

---
+++ src/assets/code_sites.tsx
@@ -0,0 +1,173 @@
+export const default_sites: { key: string; sites: { image: string; site: string }[] }[] =
+	[
+		{
+			key: 'css',
+			sites: [
+				{
+					image: 'css-tricks.png',
+					site: 'https://css-tricks.com',
+				},
+				{
+					image: 'mozilla.png',
+					site: 'https://developer.mozilla.org',
+				},
+				{
+					image: 'w3.png',
+					site: 'https://www.w3schools.com',
+				},
+				{
+					image: 'css-ref.png',
+					site: 'https://cssreference.io',
+				},
+				{
+					image: 'stack-overflow.png',
+					site: 'https://stackoverflow.com',
+				},
+				{
+					image: 'reddit.png',
+					site: 'https://www.reddit.com',
+				},
+				{
+					image: 'github.png',
+					site: 'https://github.com',
+				},
+				{
+					image: 'quora.png',
+					site: 'https://www.quora.com',
+				},
+			],
+		},
+		{
+			key: 'js',
+			sites: [
+				{
+					image: 'mozilla.png',
+					site: 'https://developer.mozilla.org',
+				},
+				{
+					image: 'W3schools.png',
+					site: 'https://w3schools.com',
+				},
+				{
+					image: 'stack.png',
+					site: 'https://stackoverflow.com',
+				},
+				{
+					image: 'reddit.png',
+					site: 'https://reddit.com',
+				},
+				{
+					image: 'github.png',
+					site: 'https://github.com',
+				},
+				{
+					image: 'quora.png',
+					site: 'https://quora.com',
+				},
+			],
+		},
+		{
+			key: 'HTML',
+			sites: [
+				{
+					image: 'mozilla.png',
+					site: 'https://developer.mozilla.org',
+				},
+				{
+					image: 'W3schools.png',
+					site: 'https://w3schools.com',
+				},
+				{
+					image: 'stack.png',
+					site: 'https://stackoverflow.com',
+				},
+				{
+					image: 'reddit.png',
+					site: 'https://reddit.com',
+				},
+				{
+					image: 'github.png',
+					site: 'https://github.com',
+				},
+				{
+					image: 'quora.png',
+					site: 'https://quora.com',
+				},
+			],
+		},
+		{
+			key: 'Python',
+			sites: [
+				{
+					image: 'stack.png',
+					site: 'https://stackoverflow.com',
+				},
+				{
+					image: 'reddit.png',
+					site: 'https://reddit.com',
+				},
+				{
+					image: 'github.png',
+					site: 'https://github.com',
+				},
+				{
+					image: 'quora.png',
+					site: 'https://quora.com',
+				},
+				{
+					image: 'docs.png',
+					site: 'https://docs.python.org',
+				},
+			],
+		},
+		{
+			key: 'C',
+			sites: [
+				{
+					image: 'stack.png',
+					site: 'https://stackoverflow.com',
+				},
+				{
+					image: 'reddit.png',
+					site: 'https://reddit.com',
+				},
+				{
+					image: 'github.png',
+					site: 'https://github.com',
+				},
+				{
+					image: 'quora.png',
+					site: 'https://quora.com',
+				},
+				{
+					image: 'gnu.png',
+					site: 'https://gnu.org',
+				},
+			],
+		},
+		{
+			key: 'PHP',
+			sites: [
+				{
+					image: 'stack.png',
+					site: 'https://stackoverflow.com',
+				},
+				{
+					image: 'reddit.png',
+					site: 'https://reddit.com',
+				},
+				{
+					image: 'github.png',
+					site: 'https://github.com',
+				},
+				{
+					image: 'quora.png',
+					site: 'https://quora.com',
+				},
+				{
+					image: 'phpnet.png',
+					site: 'https://php.net',
+				},
+			],
+		},
+	];
