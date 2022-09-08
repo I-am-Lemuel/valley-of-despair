24890f31afb12c508596def5dadb943bbf834d70
Julian Roeland <julianroeland@gmail.com>
2022-09-08 10:50:48 +0200

    s

--- pages/code/[slug].tsx
+++ pages/code/[slug].tsx
@@ -1,63 +1,63 @@
-import { NextPage } from "next";
-import { useState } from "react";
-import { code_sites } from "../../src/assets/code_sites";
-import DocumentetionBlocks from "../../src/components/DocumentationBlocks/DocumentetionBlocks";
-import Layout from "../../src/components/Layout";
-import { Search } from "../../src/components/Search/Search";
-import { SiteBlocks } from "../../src/components/SiteBlocks/SiteBlocks";
+import { NextPage } from 'next';
+import { useState } from 'react';
+import { code_sites } from '../../src/assets/code_sites';
+import { Blocks } from '../../src/components/Blocks/Blocks';
+import Layout from '../../src/components/Layout';
+import { Search } from '../../src/components/Search/Search';
 interface Props {
-  slug: string;
-  stringified_sites: string;
+	slug: string;
+	stringified_sites: string;
+	stringified_documentation_site: string;
 }
 const CodePage: NextPage<Props> = (props) => {
-  const { slug, stringified_sites } = props;
-  const all_sites: { image: string; site: string; title: string }[] =
-    JSON.parse(stringified_sites);
-  const site_params = `\"${slug}\"`;
+	const { slug, stringified_sites, stringified_documentation_site } = props;
+	const all_sites: { image: string; site: string; title: string }[] =
+		JSON.parse(stringified_sites);
+	const documentation_site: { image: string; site: string; title: string } = JSON.parse(
+		stringified_documentation_site,
+	);
+	const site_params = `\"${slug}\"`;
 
-  const [selectedSites, setSelectedSites] = useState<{
-    selected_sites: string[];
-  }>({
-    selected_sites: ["https://stackoverflow.com"],
-  });
-  return (
-    <Layout title={`PHP - ${slug}`}>
-      <Search
-        placeholder={`Search in ${slug}`}
-        key_params={site_params}
-        selectedSites={selectedSites}
-      />
-      <div className="DocumentetionBlocksContainer">
-        <SiteBlocks
-          all_sites={all_sites}
-          selectedSites={selectedSites}
-          setSelectedSites={setSelectedSites}
-        />
-        <DocumentetionBlocks />
-      </div>
-      <style>{`
-				.DocumentetionBlocksContainer {
-				display: flex;
-				flex-direction: column;
-				}
-			`}</style>
-    </Layout>
-  );
+	const [selectedSites, setSelectedSites] = useState<{
+		selected_sites: string[];
+	}>({
+		selected_sites: ['https://stackoverflow.com'],
+	});
+	return (
+		<Layout title={`PHP - ${slug}`}>
+			<Search
+				placeholder={`Search in ${slug}`}
+				key_params={site_params}
+				selectedSites={selectedSites}
+			/>
+			<Blocks
+				documentation_site={documentation_site}
+				all_sites={all_sites}
+				selectedSites={selectedSites}
+				setSelectedSites={setSelectedSites}
+			/>
+		</Layout>
+	);
 };
 
 export async function getServerSideProps(context: any) {
-  const { slug } = context.params;
+	const { slug } = context.params;
 
-  const stringified_sites = JSON.stringify(
-    code_sites.filter((site) => site.key === slug)[0].sites
-  );
+	const stringified_sites = JSON.stringify(
+		code_sites.filter((site) => site.key === slug)[0].sites,
+	);
 
-  return {
-    props: {
-      slug: slug,
-      stringified_sites: stringified_sites,
-    },
-  };
+	const stringified_documentation_site = JSON.stringify(
+		code_sites.filter((site) => site.key === slug)[0].documentation_site,
+	);
+
+	return {
+		props: {
+			slug: slug,
+			stringified_sites: stringified_sites,
+			stringified_documentation_site: stringified_documentation_site,
+		},
+	};
 }
 
 export default CodePage;
--- src/assets/code_sites.tsx
+++ src/assets/code_sites.tsx
@@ -1,210 +1,253 @@
 export const code_sites: {
 	key: string;
-	sites: { image: string; site: string; title: string }[];
+	sites: { image: string; site: string; title: string; default?: boolean }[];
+	documentation_site: { image: string; site: string; title: string };
 }[] = [
-		{
-			key: 'css',
-			sites: [
-				{
-					image: 'css-tricks.png',
-					site: 'https://css-tricks.com',
-					title: 'CSS-Tricks',
-				},
-				{
-					image: 'mozilla.png',
-					site: 'https://developer.mozilla.org',
-					title: 'Mozilla Developer Network',
-				},
-				{
-					image: 'w3.png',
-					site: 'https://www.w3schools.com',
-					title: 'W3Schools',
-				},
-				{
-					image: 'css-ref.png',
-					site: 'https://cssreference.io',
-					title: 'CSS Reference',
-				},
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-					title: 'Stack Overflow',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://www.reddit.com',
-					title: 'Reddit',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-					title: 'Github',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://www.quora.com',
-					title: 'Quora',
-				},
-			],
+	{
+		key: 'css',
+		sites: [
+			{
+				image: 'css-tricks.png',
+				site: 'https://css-tricks.com',
+				title: 'CSS-Tricks',
+			},
+			{
+				default: true,
+				image: 'mozilla.png',
+				site: 'https://developer.mozilla.org',
+				title: 'Mozilla Developer Network',
+			},
+			{
+				image: 'w3.png',
+				site: 'https://www.w3schools.com',
+				title: 'W3Schools',
+			},
+			{
+				image: 'css-ref.png',
+				site: 'https://cssreference.io',
+				title: 'CSS Reference',
+			},
+			{
+				default: true,
+				image: 'stack.png',
+				site: 'https://stackoverflow.com',
+				title: 'Stack Overflow',
+			},
+			{
+				image: 'reddit.png',
+				site: 'https://www.reddit.com',
+				title: 'Reddit',
+			},
+			{
+				image: 'github.png',
+				site: 'https://github.com',
+				title: 'Github',
+			},
+			{
+				image: 'quora.png',
+				site: 'https://www.quora.com',
+				title: 'Quora',
+			},
+		],
+		documentation_site: {
+			title: '',
+			image: '',
+			site: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
 		},
-		{
-			key: 'js',
-			sites: [
-				{
-					image: 'mozilla.png',
-					site: 'https://developer.mozilla.org',
-					title: 'Mozilla Developer Network',
-				},
-				{
-					image: 'w3.png',
-					site: 'https://w3schools.com',
-					title: 'W3Schools',
-				},
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-					title: 'Stack Overflow',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-					title: 'Reddit',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-					title: 'Github',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-					title: 'Quora',
-				},
-			],
+	},
+	{
+		key: 'js',
+		sites: [
+			{
+				default: true,
+				image: 'mozilla.png',
+				site: 'https://developer.mozilla.org',
+				title: 'Mozilla Developer Network',
+			},
+			{
+				image: 'w3.png',
+				site: 'https://w3schools.com',
+				title: 'W3Schools',
+			},
+			{
+				default: true,
+				image: 'stack.png',
+				site: 'https://stackoverflow.com',
+				title: 'Stack Overflow',
+			},
+			{
+				image: 'reddit.png',
+				site: 'https://reddit.com',
+				title: 'Reddit',
+			},
+			{
+				image: 'github.png',
+				site: 'https://github.com',
+				title: 'Github',
+			},
+			{
+				image: 'quora.png',
+				site: 'https://quora.com',
+				title: 'Quora',
+			},
+		],
+		documentation_site: {
+			title: '',
+			image: '',
+			site: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
 		},
-		{
-			key: 'html',
-			sites: [
-				{
-					image: 'mozilla.png',
-					site: 'https://developer.mozilla.org',
-					title: 'Mozilla Developer Network',
-				},
-				{
-					image: 'w3.png',
-					site: 'https://w3schools.com',
-					title: 'W3Schools',
-				},
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-					title: 'Stack Overflow',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-					title: 'Reddit',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-					title: 'Github',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-					title: 'Quora',
-				},
-			],
+	},
+	{
+		key: 'html',
+		sites: [
+			{
+				default: true,
+				image: 'mozilla.png',
+				site: 'https://developer.mozilla.org',
+				title: 'Mozilla Developer Network',
+			},
+			{
+				image: 'w3.png',
+				site: 'https://w3schools.com',
+				title: 'W3Schools',
+			},
+			{
+				default: true,
+				image: 'stack.png',
+				site: 'https://stackoverflow.com',
+				title: 'Stack Overflow',
+			},
+			{
+				image: 'reddit.png',
+				site: 'https://reddit.com',
+				title: 'Reddit',
+			},
+			{
+				image: 'github.png',
+				site: 'https://github.com',
+				title: 'Github',
+			},
+			{
+				image: 'quora.png',
+				site: 'https://quora.com',
+				title: 'Quora',
+			},
+		],
+		documentation_site: {
+			title: '',
+			image: '',
+			site: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
 		},
-		{
-			key: 'python',
-			sites: [
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-					title: 'Stack Overflow',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-					title: 'Reddit',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-					title: 'Github',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-					title: 'Quora',
-				},
-				{
-					image: 'python-docs.png',
-					site: 'https://docs.python.org',
-					title: 'Python Docs',
-				},
-			],
+	},
+	{
+		key: 'python',
+		sites: [
+			{
+				default: true,
+				image: 'stack.png',
+				site: 'https://stackoverflow.com',
+				title: 'Stack Overflow',
+			},
+			{
+				image: 'reddit.png',
+				site: 'https://reddit.com',
+				title: 'Reddit',
+			},
+			{
+				image: 'github.png',
+				site: 'https://github.com',
+				title: 'Github',
+			},
+			{
+				image: 'quora.png',
+				site: 'https://quora.com',
+				title: 'Quora',
+			},
+			{
+				default: true,
+				image: 'python-docs.png',
+				site: 'https://docs.python.org',
+				title: 'Python Docs',
+			},
+		],
+		documentation_site: {
+			title: '',
+			image: '',
+			site: 'https://docs.python.org/3/',
 		},
-		{
-			key: 'c',
-			sites: [
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-					title: 'Stack Overflow',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-					title: 'Reddit',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-					title: 'Github',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-					title: 'Quora',
-				},
-				{
-					image: 'gnu.png',
-					site: 'https://gnu.org',
-					title: 'GNU',
-				},
-			],
+	},
+	{
+		key: 'c',
+		sites: [
+			{
+				default: true,
+				image: 'stack.png',
+				site: 'https://stackoverflow.com',
+				title: 'Stack Overflow',
+			},
+			{
+				image: 'reddit.png',
+				site: 'https://reddit.com',
+				title: 'Reddit',
+			},
+			{
+				image: 'github.png',
+				site: 'https://github.com',
+				title: 'Github',
+			},
+			{
+				image: 'quora.png',
+				site: 'https://quora.com',
+				title: 'Quora',
+			},
+			{
+				default: true,
+				image: 'gnu.png',
+				site: 'https://gnu.org',
+				title: 'GNU',
+			},
+		],
+		documentation_site: {
+			title: '',
+			image: '',
+			site: 'https://www.gnu.org/software/gnu-c-manual/gnu-c-manual.html',
 		},
-		{
-			key: 'php',
-			sites: [
-				{
-					image: 'stack.png',
-					site: 'https://stackoverflow.com',
-					title: 'Stack Overflow',
-				},
-				{
-					image: 'reddit.png',
-					site: 'https://reddit.com',
-					title: 'Reddit',
-				},
-				{
-					image: 'github.png',
-					site: 'https://github.com',
-					title: 'Github',
-				},
-				{
-					image: 'quora.png',
-					site: 'https://quora.com',
-					title: 'Quora',
-				},
-				{
-					image: 'phpnet.png',
-					site: 'https://php.net',
-					title: 'PHP.net',
-				},
-			],
+	},
+	{
+		key: 'php',
+		sites: [
+			{
+				default: true,
+				image: 'stack.png',
+				site: 'https://stackoverflow.com',
+				title: 'Stack Overflow',
+			},
+			{
+				image: 'reddit.png',
+				site: 'https://reddit.com',
+				title: 'Reddit',
+			},
+			{
+				image: 'github.png',
+				site: 'https://github.com',
+				title: 'Github',
+			},
+			{
+				image: 'quora.png',
+				site: 'https://quora.com',
+				title: 'Quora',
+			},
+			{
+				default: true,
+				image: 'phpnet.png',
+				site: 'https://php.net',
+				title: 'PHP.net',
+			},
+		],
+		documentation_site: {
+			title: '',
+			image: '',
+			site: 'https://www.php.net/manual/en/intro-whatis.php',
 		},
-	];
+	},
+];
