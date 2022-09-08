ea5b4ce899cccfcff5691926a886e05894edc621
SudoLemuel <lembak03@hotmail.com>
2022-09-08 11:32:10 +0200

    format 3

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
-import { FunctionComponent } from 'react';
-import { StyledHeader, StyledText } from './StyledHeader';
+import { FunctionComponent } from "react";
+import { StyledHeader, StyledText } from "./StyledHeader";
 
 interface Props {}
 
 const Header: FunctionComponent<Props> = (props: any) => {
-	return (
-		<StyledHeader>
-			<StyledText>
-				<p>Valley of Despair</p>
-			</StyledText>
-		</StyledHeader>
-	);
+  return (
+    <StyledHeader>
+      <StyledText>
+        <p>Valley of Despair</p>
+      </StyledText>
+    </StyledHeader>
+  );
 };
 export default Header;
