d39b1101b0434dc7181fb6f9c2f30aa31cebcc21
Niek Goedemans <niekgoedemans93@gmail.com>
2022-09-08 11:01:35 +0200

    gg

--- src/components/SiteBlocks/StyledSiteBlocks.tsx
+++ src/components/SiteBlocks/StyledSiteBlocks.tsx
@@ -9,20 +9,24 @@ export const StyledSiteBlocks = styled.div`
 `;
 export const StyledSiteBtn = styled.div<{ selected: boolean }>`
   transition: 0.4s;
-  margin: 2.5rem;
+  margin: 1.3rem;
+  width: 170px;
   ${({ selected }) =>
     selected &&
     `
-    border: 2px solid #000;
+    color: #ffff;
+    background-color: #6E2594;
+    border: 2px solid #000000;
     `}
   :hover {
     transition: 0.4s;
     border-radius: 15px;
-    background-color: lightgrey;
+    border: solid 1px black;
     transform: scale(1.2);
     -webkit-transform: scale(1.2);
     -ms-transform: scale(1.2);
     outline: none;
+    cursor: pointer;
   }
 `;
 export const StyledSiteTitle = styled.div`
